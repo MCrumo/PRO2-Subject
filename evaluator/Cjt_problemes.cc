20a19dc566959704193cecdf43e55989ec18d0dc
MCrumo <miquel1rr@gmail.com>
2021-04-22 13:50:22 +0200

    especificacio

---
+++ evaluator/Cjt_problemes.cc
@@ -0,0 +1,29 @@
+#include "Cjt_problemes.hh"
+
+Cjt_problemes::Cjt_problemes() {
+    
+}
+
+void    Cjt_problemes::afegir_problema(const Problema& p) {
+    //map
+}
+
+bool Cjt_problemes::existeix_problema(string nom) {
+    //map
+}
+
+Problema Cjt_problemes::buscar_problema(string nom) {
+    //map
+}
+
+void Cjt_problemes::llegir_problemes() {
+    
+}
+
+void Cjt_problemes::escriure_problemes() {
+    
+}
+
+void Cjt_problemes::llistar_problemes() {
+    
+}
