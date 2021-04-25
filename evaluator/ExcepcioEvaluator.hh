652eb0c5c2857aea4c7cbe8f55cc12ba0682a095
MCrumo <miquel1rr@gmail.com>
2021-04-25 13:43:58 +0200

    Errors evaluator

---
+++ evaluator/ExcepcioEvaluator.hh
@@ -0,0 +1,20 @@
+#ifndef EXCEPCIO_EVALUATOR
+#define EXCEPCIO_EVALUATOR
+
+#ifndef NO_DIAGRAM
+#include <exception>
+#endif
+
+using namespace std;
+
+// Classe ExcepcioEvaluator
+class ExcepcioEvaluator: public exception 
+{
+public:
+    ExcepcioEvaluator(const char* frase) : exception(), missatge(frase) {}
+    const char* what() const throw() {return missatge;}; 
+private:
+    const char* missatge;
+};
+
+#endif
