53e1dec8461c58029538b4c1bdd2ab0b1a592221
MCrumo <80285362+MCrumo@users.noreply.github.com>
2021-06-13 17:45:53 +0200

    Classe PILA IO

--- classes/Pila.hh
+++ classes/Pila.hh
@@ -120,6 +120,28 @@ public:
   {
     return altura;
   }
+  
+  // Input Output
+  void llegir_pila_int(Pila<int> &p, int marca)
+{
+  int n;
+  cin >> n;
+  while (n != marca){
+    p.empilar(n);
+    cin >> n;
+  }
+}
+
+void escriure_pila_int(const Pila<int> &p)
+{
+  cout << "[Cim] " << endl;
+  Pila<int> copia(p); // copiamos p para que no se destruya y no tener
+  while(not copia.es_buida()){ //que acordarnos de copiarla fuera
+    cout << copia.cim() << endl;
+    copia.desempilar(); // el que no haya salto de linea solo interesa
+  }                   // si la pila no es muy grande
+  cout << "[Ultim]"<<endl;
+}
 
  #include "program.hh"
 };
